3567933a860b8cbffc6338e240be60c1578e1365
daniel <daniel.gj.tan@gmail.com>
2020-07-29 18:48:05 +0800

    update test

--- bar_test.go
+++ bar_test.go
@@ -1,7 +1,7 @@
 /**
  * @ Author: Daniel Tan
  * @ Date: 2020-07-29 13:45:28
- * @ LastEditTime: 2020-07-29 18:47:16
+ * @ LastEditTime: 2020-07-29 18:47:56
  * @ LastEditors: Daniel Tan
  * @ Description:
  * @ FilePath: /bar/bar_test.go
@@ -18,20 +18,20 @@ func TestBar(t *testing.T) {
 	b := NewBar(0, 100)
 	b.Cur <- CurrentStep{
 		Cur:      50,
-		Message:  fmt.Sprintf("step %v", 88),
+		Message:  fmt.Sprintf("step %v", 50),
 		IsReturn: true,
 	}
 	b.Cur <- CurrentStep{
 		Cur:     86,
-		Message: fmt.Sprintf("step %v", 88),
+		Message: fmt.Sprintf("step %v", 86),
 	}
 	b.Cur <- CurrentStep{
 		Cur:     87,
-		Message: fmt.Sprintf("step %v", 88),
+		Message: fmt.Sprintf("step %v", 87),
 	}
 	b.Cur <- CurrentStep{
 		Cur:     100,
-		Message: fmt.Sprintf("step %v", 88),
+		Message: fmt.Sprintf("step %v", 100),
 	}
 
 }
